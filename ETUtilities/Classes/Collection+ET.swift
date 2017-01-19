24bc41761136ee95e75d757dcd311a991fd8c527
Jan Cislinsky <jan.cislinsky@etnetera.cz>
2017-01-19 09:33:35 +0100

    * Collection extension (et_remove(:)) (objc-rewrite)

--- ETUtilities/Classes/Collection+ET.swift
+++ ETUtilities/Classes/Collection+ET.swift
@@ -25,3 +25,63 @@ public extension Collection where Indices.Iterator.Element == Index {
         return indices.contains(index) ? self[index] : nil
     }
 }
+
+public extension RangeReplaceableCollection where Iterator.Element: Equatable {
+
+    /// Removes and returns specified element.
+    ///
+    /// All the elements following the specified element are moved to close the
+    /// gap. This example removes the middle element from an array of
+    /// measurements.
+    ///
+    ///     var measurements = [1.2, 1.5, 2.9, 1.1, 1.6]
+    ///     let removed = measurements.et_remove(2.9)
+    ///     print(measurements)
+    ///     // Prints "[1.2, 1.5, 1.2, 1.6]"
+    ///
+    /// Calling this method may invalidate any existing indices for use with this
+    /// collection.
+    ///
+    /// - Parameter element: The element to remove.
+    /// - Returns: The removed element.
+    ///
+    /// - Complexity: O(*n*), where *n* is the length of the collection.
+    ///
+    /// - Author: Jan Cislinsky
+    /// - Since: 01/2017
+    mutating func et_remove(_ element: Self.Iterator.Element) -> Self.Iterator.Element {
+        
+        // Element never exists in collection
+        guard let idx = index(of: element) else { return element }
+
+        // Removes element at index
+        return remove(at: idx)
+    }
+
+    /// Removes specified elements from the collection.
+    ///
+    /// All the elements following the specified elements are moved to close the
+    /// gap. This example removes the first part of an array of
+    /// measurements.
+    ///
+    ///     var measurements = [1.2, 1.5, 2.9, 1.1, 1.6]
+    ///     let removed = measurements.et_remove([1.2, 1.5, 2.9])
+    ///     print(measurements)
+    ///     // Prints "[1.1, 1.6]"
+    ///
+    /// Calling this method may invalidate any existing indices for use with this
+    /// collection.
+    ///
+    /// - Parameter elements: The array of elements to remove.
+    ///
+    /// - Complexity: O(*m* * *n*), where *m* is the length of the collection of
+    /// specified elements and *n* is the length of the collection.
+    ///
+    /// - Author: Jan Cislinsky
+    /// - Since: 01/2017
+    mutating func et_remove(_ elements: [Self.Iterator.Element]) {
+
+        // Removes all elements one by one
+        elements.forEach { et_remove($0) }
+    }
+}
--- Example/Tests/CollectionTests.swift
+++ Example/Tests/CollectionTests.swift
@@ -7,17 +7,89 @@ import ETUtilities
 class Collection: QuickSpec {
     override func spec() {
         
-        describe("index out of range") {
+        describe("et_safe subscript") {
 
             var model:[Int]!
             beforeEach {
                 model = [0]
             }
 
-            context("use safe subscript") {
+            context("index out of range") {
 
                 it("read") {
                     expect(model[et_safe: 1]).to(beNil())
+                }
+            }
+        }
+
+        describe("et_remove") {
+
+            var measurements:[Double]!
+            beforeEach {
+                measurements = [1.2, 1.5, 2.9, 1.1, 1.6]
+            }
+
+            context("single element") {
+
+                it("middle") {
+                    let removed = measurements.et_remove(2.9)
+                    expect(removed).to(equal(2.9))
+                    expect(measurements.count).to(equal(4))
+                    expect(measurements).to(contain([1.2, 1.5, 1.1, 1.6]))
+                }
+
+                it("first") {
+                    let removed = measurements.et_remove(1.2)
+                    expect(removed).to(equal(1.2))
+                    expect(measurements.count).to(equal(4))
+                    expect(measurements).to(contain([1.5, 2.9, 1.1, 1.6]))
+                }
+
+                it("last") {
+                    let removed = measurements.et_remove(1.6)
+                    expect(removed).to(equal(1.6))
+                    expect(measurements.count).to(equal(4))
+                    expect(measurements).to(contain([1.2, 1.5, 2.9, 1.1]))
+                }
+
+                it("non existing") {
+                    let removed = measurements.et_remove(10.0)
+                    expect(removed).to(equal(10.0))
+                    expect(measurements.count).to(equal(5))
+                    expect(measurements).to(contain([1.2, 1.5, 2.9, 1.1, 1.6]))
+                }
+            }
+
+            context("multiple elements") {
+
+                it("middle") {
+                    measurements.et_remove([1.1, 2.9, 1.5])
+                    expect(measurements.count).to(equal(2))
+                    expect(measurements).to(contain([1.2, 1.6]))
+                }
+
+                it("beginning") {
+                    measurements.et_remove([1.2, 1.5])
+                    expect(measurements.count).to(equal(3))
+                    expect(measurements).to(contain([2.9, 1.1, 1.6]))
+                }
+
+                it("end") {
+                    measurements.et_remove([1.6, 1.1])
+                    expect(measurements.count).to(equal(3))
+                    expect(measurements).to(contain([1.2, 1.5, 2.9]))
+                }
+
+                it("non existing") {
+                    measurements.et_remove([10.0, 15.0])
+                    expect(measurements.count).to(equal(5))
+                    expect(measurements).to(contain([1.2, 1.5, 2.9, 1.1, 1.6]))
+                }
+
+                it("all in one") {
+                    measurements.et_remove([10.0, 1.2, 2.9, 32.0, 1.6])
+                    expect(measurements.count).to(equal(2))
+                    expect(measurements).to(contain([1.5, 1.1]))
                 }
             }
         }
